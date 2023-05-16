b3b507f64b3ddd0bbe0aa66fcbecabd451a014bd
Darya2477 <85761375+Darya2477@users.noreply.github.com>
2023-05-16 14:23:20 +0300

    Create user_connection.cpp

---
+++ db/server_connector/user_connection.cpp
@@ -0,0 +1,14 @@
+#include "user_connection.hpp"
+
+DB_Connection::DB_Connection(){
+    db_connection.reset( PQsetdbLogin(db_host.c_str(),db_port.c_str(), nullptr, nullptr, db_name.c_str(), db_user.c_str(), db_password.c_str()), &PQfinish); //установка соединения с бд
+    if (PQstatus(db_connection.get())!=CONNECTION_OK && PQsetnonblocking(db_connection.get(), 1)!=0) {  //проверка успешности установки соединения и установка неблокирующего режима соединения
+        throw std::runtime_error(PQerrorMessage(db_connection.get()));
+    }
+
+}
+
+std::shared_ptr<user_connect> DB_Connection::connection() const //возврат указателя на текущее соединение
+{
+    return db_connection;
+}
