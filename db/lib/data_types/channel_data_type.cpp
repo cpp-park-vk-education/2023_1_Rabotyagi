bbc19775032bdf33d902a232c8ec554280272c4b
Darya2477 <85761375+Darya2477@users.noreply.github.com>
2023-05-19 17:48:27 +0300

    Update channel_data_type.cpp

--- db/lib/data_types/channel_data_type.cpp
+++ db/lib/data_types/channel_data_type.cpp
@@ -1,9 +1,9 @@
-struct Channel
+struct Channel:Base
 {
-    unsigned int id;
-    unsigned int guild_id;
-    unsigned int name;
-    std::string type;
-    std::string created_at;
-    std::string updated_at;
+    unsigned int id=0;
+    unsigned int guild_id=0;
+    std::string name="";
+    std::string type="";
+    std::string created_at="";
+    std::string updated_at="";
 };
