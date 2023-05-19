de0a5cae750eaeee52b8dc24d5cc58eb9f9c38c8
Darya2477 <85761375+Darya2477@users.noreply.github.com>
2023-05-20 01:11:10 +0300

    Update message.cpp

--- db/lib/tables/message.cpp
+++ db/lib/tables/message.cpp
@@ -1,70 +1,97 @@
-class Message : DatabaseTable
+class MessageTable : DatabaseTable
 {
-    Message("messages")
+    MessageTable("messages")
     {
     }
-    ~Message()
+    ~MessageTable()
     {
     }
-    void initRecord(unsigned int id, unsigned int owner_id, unsigned int channel_id, const QString &content, const QString &created_at, const QString &updated_at);
-    bool insertRecord() override
-    {
-        QSqlQuery query(db);
-        query.prepare("INSERT INTO " + tableName + " (id,owner_id, channel_id, content, created_at, updated_at) VALUES (:id, :owner_id, :channel_id, :content, :created_at, :updated_at)");
-        query.bindValue(":id", _id);
-        query.bindValue(":owner_id", _owner_id);
-        query.bindValue(":channel_id", _channel_id);
-        query.bindValue(":content", _content);
-        query.bindValue(":created_at", _created_at);
-        query.bindValue(":updated_at", _updated_at);
-        return query.exec();
+    bool Checker(unsigned int owner_id, unsigned int channel_id){
+        pqxx::work transaction(*conn);
+        std::string sql="SELECT COUNT(*) FROM users";
+        pqxx::result user_count=transaction.exec(sql);
+        sql="SELECT COUNT(*) FROM channels";
+        pqxx::result channel_count=transaction.exec(sql);
+        transaction.commit();
+        if (owner_id<=user_count.as<int>() && channel_id<=channel_count.as<int>()) return true;
+        else return false;
     }
-    bool deleteRecord() override
+    bool insertRecord(Message& message_struct) override //добавить проверку на существование канала и обладателя
     {
-        QSqlQuery query(db);
-        query.prepare("DELETE FROM " + tableName + " WHERE id=:id");
-        query.bindValue(":id", _id);
-        return query.exec();
-    }
-    bool updateRecord() override
-    {
-        QSqlQuery query(db);
-        query.prepare("UPDATE " + tableName + " SET content=:content, updated_at=:updated_at WHERE id=:id");
-        query.bindValue(":content", _content);
-        query.bindValue(":updated_at", _updated_at);
-        return query.exec();
-    }
-    QList<QVariantList> readRecord()
-    {
-        QList<QVariantList> result;
-        QSqlQuery query(db);
-        query.prepare("SELECT * FROM " + tableName);
-        if (query.exec())
-        {
-            while (query.next())
+        if (Checker(message_struct.owner_id, message_struct.channel_id)){
+            try{
+                pqxx::work transaction(*conn);
+                std::string sql = "INSERT INTO " + tableName + "(owner_id,channel_id,content,created_at,updated_at) VALUES ($1,$2,$3,$4,$5)";
+                transaction.exec_params(sql, message_struct.owner_id, message_struct.channel_id, message_struct.content, message_struct.created_at, message_struct.updated_at);
+                transaction.commit();
+                std::cout << "Record was added to db!" << std::endl;
+                return true;
+            }
+            catch (const std::exception& e) 
             {
-                QVariantList record;
-                record.append(query.value("id"));
-                record.append(query.value("owner_id"));
-                record.append(query.value("channel_id"));
-                record.append(query.value("content"));
-                record.append(query.value("created_at"));
-                record.append(query.value("updated_at"));
-                result.append(record);
+                std::cerr << e.what() << std::endl;
+                return false;
             }
         }
-        else
-        {
-            qDebug() << "Failed to select records:" << query.lastError().text();
-        }
-        return result;
+        return false;
     }
+    bool deleteRecord(unsigned int id) override
+    { 
+        try{
+            pqxx::work transaction(*conn);
+            std::string sql="DELETE FROM "+ tableName + " WHERE id=$1";
+            transaction.exec_params(sql, id );
+            transaction.commit();
+            std::cout << "Record was deleted from db!" << std::endl;
+            return true;
 
-private:
-    unsigned int _id,
-        unsigned int _owner_id,
-        unsigned int _channel_id,
-        QString _content,
-        QString _created_at,
-        QString _updated_at;
+        }
+        catch (const std::exception& e) 
+        {
+            std::cerr << e.what() << std::endl;
+            return false;
+        }
+    }
+    bool updateRecord(Message& message_struct, unsigned int id) override
+    {
+        try{
+            pqxx::work transaction(*conn);
+            std::string sql="UPDATE " + tableName + " SET content=$1, updated_at=$2 WHERE id=$3";
+            transaction.exec_params(sql, message_struct.content, message_struct.updated_at, id);
+            transaction.commit();
+            std::cout << "Record was updated!" << std::endl;
+            return true;
+        }
+        catch (const std::exception& e) 
+        {
+            std::cerr << e.what() << std::endl;
+            return false;
+        }
+    }
+    Message readRecord(unsigned int id)
+    {
+        try{
+            pqxx::work transaction(*conn);
+            std::string sql="SELECT * FROM " + tableName + " WHERE id=$1";
+            pqxx::result result = transaction.exec_params(sql, id);
+            transaction.commit();
+            Message message_result;
+            message_result.id=result[0].as<int>();
+            message_result.owner_id=result[1].as<int>();
+            message_result.channel_id=result[2].as<int>();
+            message_result.content=result[3].as<std::string>();
+            message_result.created_at=result[4].as<std::string>();
+            message_result.updated_at=result[5].as<std::string>();
+            std::cout << "Record was found!" << std::endl;
+            return message_result;
+
+        }
+        catch (const std::exception& e) 
+        {
+            std::cerr << e.what() << std::endl;
+            Message message_result;
+            return message_result; //возвращает пустой шаблон
+        }
+    }
 }
+
